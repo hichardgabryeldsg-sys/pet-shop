b0545f994ccdd5328e81999817efbcd69dc10f58
prof <prof.podecolocaraihb@gmail.com>
2026-09-10 14:11:39 -0300

    Vamos ver

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# PetShop
+# TesteProf
 
-This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 22.1.5.
+This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.2.22.
 
 ## Development server
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Component, signal } from "@angular/core";
+import { RouterOutlet, RouterLink } from "@angular/router";
+import { Login } from "./login/login";
+import { Cadastro } from "./cadastro/cadastro";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  selector: "app-root",
+  imports: [RouterOutlet, RouterLink, Login, Cadastro],
+  templateUrl: "./app.html",
+  styleUrl: "./app.css"
 })
 export class App {
-  protected readonly title = 'happyPet';
-
+  protected readonly title = signal("pet-shop");
 }
