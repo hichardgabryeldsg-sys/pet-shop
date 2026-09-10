383adfd8fc1ebc6fb2d0de3474598c2b260bf310
alexandre6677 <al4879980@gmail.com>
2026-09-10 14:26:23 -0300

    feat: implement product catalog, cart functionality, and product detail pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,36 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Cadastro } from './cadastro/cadastro';
+import { Catalog } from './pages/catalog/catalog';
+import { ProductDetail } from './pages/product-detail/product-detail';
+
+
 
 
 export const routes: Routes = [
+     {
+        path: "",
+        redirectTo: "catalog",
+        pathMatch: "full"
+       
+    },
+    {
+        path: "product/:id",
+        component: ProductDetail
+    },
+
     {
         path: "login",
         component: Login
     },
+
     {
         path: "cadastro",
         component: Cadastro
     },
     {
-        path: "",
-        pathMatch: "full"
+        path: "catalog",
+        component: Catalog
     }
+   
 ];
